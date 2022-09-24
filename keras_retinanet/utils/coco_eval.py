e203bcf31ae4f1e289c01201d098daabc187dcce
FATAUNDERWARE <114299128+FATAUNDERWARE@users.noreply.github.com>
2022-09-24 22:06:01 +0600

    Update coco_eval.py

--- keras_retinanet/utils/coco_eval.py
+++ keras_retinanet/utils/coco_eval.py
@@ -19,7 +19,7 @@ from pycocotools.cocoeval import COCOeval
 from tensorflow import keras
 import numpy as np
 import json
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 import progressbar
 assert(callable(progressbar.progressbar)), "Using wrong progressbar module, install 'progressbar2' instead."
